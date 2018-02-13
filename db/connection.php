3fa75a7ab832a84fa14cca3473f4366e61bef74a
Shamsaagazarzoo M Shaikh <arzoo.rkcet@gmail.com>
2018-02-13 09:15:59 +0530

    update

--- db/connection.php
+++ db/connection.php
@@ -1,12 +1,12 @@
 <?php
 //---------------------------mysql db connection ------------//
 //include('db/connection.php');
-$hostname='localhost';
-$username='root';
-$password='';
+$hostname='us-cdbr-iron-east-05.cleardb.net';
+$username='b9f4b048546dd8';
+$password='4aa15ad9';
 
 try {
-    $dbh = new PDO("mysql:host=$hostname;dbname=lawoffice",$username,$password);
+    $dbh = new PDO("mysql:host=$hostname;dbname=heroku_e347d42b789f20a",$username,$password);
 //    echo 'Connected to Database<br/>';
     }
 catch(PDOException $e)
