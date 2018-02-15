663d7df8a54748fb387e9844fe679ded80afeeb3
Shamsaagazarzoo M Shaikh <arzoo.rkcet@gmail.com>
2018-02-15 18:57:03 +0530

    update

--- js/callcenter.js
+++ js/callcenter.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
+	load_all_tables();
 	$(document).on("change","#opt",function(e){
 		$('#payment_table').hide();
+		clear_payment_form();
 		var id=document.getElementById('opt').value;
 		//alert(id);
-		if(id==1){
+		if(id==0)
+		{
+			$('#payment_table').hide();
+		}
+		else if(id==1){
 			$('#customer_called_form').show();
 			$('#report_form').hide();
 			$('#payment_form').hide();
@@ -110,7 +116,7 @@ $(document).ready(function(){
 				})		
 				//$().toastmessage('showSuccessToast',data);
 				clear_payment_form();
-				$('#payment_form').hide();
+				//$('#payment_form').hide();
 				$('#opt').val(0);
 				load_payment_table(ins_no);
 			}
@@ -126,9 +132,61 @@ function clear_form()
 	
 }
 
-function load_payment_table($no)
+function load_all_tables()
 {
-	var ins_no1=$no;
+	var rep1='all';
+	$.ajax({
+			
+			type:"post",
+			url:"ajax/report_data.php",
+			data:{'rep1':rep1},
+			success:function(data)
+			{
+				$("#rep_body").html(data);
+				
+			//alert(data);
+				// $('#msg').fadeIn('slow',function(){
+				// $('#msg').html(data);
+				// $('#msg').delay(1000).fadeOut();
+				// })		
+				//$().toastmessage('showSuccessToast',data);
+				// clear_form();
+				// $('#report_form').hide();
+				// $('#opt').val(0);
+			}
+		});
+		
+	$.ajax({
+			
+			type:"post",
+			url:"ajax/report_data.php",
+			data:{'rep2':rep1},
+			success:function(data)
+			{
+				$("#rep2_body").html(data);
+				
+			//alert(data);
+
+			}
+		});
+		
+		$.ajax({
+			
+			type:"post",
+			url:"ajax/report_data.php",
+			data:{'rep3':rep1},
+			success:function(data)
+			{
+				$("#rep3_body").html(data);
+				
+			//alert(data);
+			
+			}
+		});	
+}
+function load_payment_table(no)
+{
+	var ins_no1=no;
 		$.ajax({
 			
 			type:"post",
@@ -136,8 +194,22 @@ function load_payment_table($no)
 			data:{'ins_no1':ins_no1},
 			success:function(data)
 			{
-				$("#t_body").html(data);
-				$("#payment_table").show();
+				if(data==0)
+				{
+					$('#search_no').html('');
+					$('#ins_name').val('');
+					$('#msg_ins').html('Institution Number Does Not Exist !!!, Please Enter From Institution Master');
+				}
+				else
+				{
+					$('#search_no').html(ins_no1);
+					$('#msg_ins').html('');
+					$("#t_body").html(data);
+					$("#payment_table").show();
+					var inst_nm = $('#example').find("td").eq(2).html(); 
+					$('#ins_name').val(inst_nm);	
+				}					
+								
 					// alert(data);
 				// $('#msg').fadeIn('slow',function(){
 				// $('#msg').html(data);
@@ -149,6 +221,8 @@ function load_payment_table($no)
 				// $('#opt').val(0);
 			}
 		});
+		
+		
 }
 
 function clear_payment_form()
@@ -164,4 +238,51 @@ function clear_payment_form()
 	$('#search_no').html('');
 	$('#t_body').html('');
 	
+}
+function validate()
+{
+	//var staff_id=document.getElementById('staff').value;
+		var f_date=$("#frmdt").val();
+		var t_date=$("#todt").val();
+		if(f_date!='')
+		{
+			
+		
+			if(t_date!='')
+			{
+				
+			return true;
+				// if(staff_id!=0)
+				// {
+					// return true;
+				// }
+				// else
+				// {
+					// $('#s_nm').fadeIn('slow',function(){
+					// $('#s_nm').html("please select staff name");
+					// $('#s_nm').delay(1500).fadeOut();
+					// })
+					// return false;
+				// }
+			}
+			else
+			{
+				$('#t_d').fadeIn('slow',function(){
+				$('#t_d').html("please select Ending Date");
+				$('#t_d').delay(1500).fadeOut();
+				})
+				
+				return false;
+			}
+		}
+		else
+		{
+				$('#f_d').fadeIn('slow',function(){
+				$('#f_d').html("please select starting date");
+				$('#f_d').delay(1500).fadeOut();
+				})
+			
+			return false;
+		}
+		
 }
